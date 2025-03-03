5a8220a079e70acd6693ea897aca3ddb9c9be82b
luisdanielta <luisdaniel.ta@outlook.com>
2025-03-03 07:02:02 +0000

    feat: Add Plotly.js and react-plotly.js dependencies, enhance NavBar styling with shadow effect

--- web/src/components/ui/navBar.tsx
+++ web/src/components/ui/navBar.tsx
@@ -7,12 +7,9 @@ export default function NavBar() {
         <h1 className="font-bold text-xl text-green-700">XRounting</h1>
 
         <div className="flex items-center gap-4">
-
-          <section className="flex items-center gap-2 border-2 border-green-300 py-1 px-4 rounded-full hover:bg-green-100 cursor-pointer transition-colors duration-200 ease-in hover:ease-out">
+          <section className="flex items-center gap-2 border-2 border-green-300 py-1 px-4 rounded-full hover:bg-green-100 cursor-pointer transition-colors duration-200 ease-in hover:ease-out shadow-md shadow-green-100">
             <p className="text-sm font-bold">John Doe</p>
           </section>
-
-
 
           <div className="text-red-700 cursor-pointer">
             <ISignOut />
